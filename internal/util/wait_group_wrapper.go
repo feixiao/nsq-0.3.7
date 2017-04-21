74fe0c467d7b8570db182ca2f9d5b0a2d8093c97
frank <feixiao2020@sina.com>
2017-04-21 10:14:25 +0800

    Update wait_group_wrapper.go

--- internal/util/wait_group_wrapper.go
+++ internal/util/wait_group_wrapper.go
@@ -4,10 +4,12 @@ import (
 	"sync"
 )
 
+// 封装sync.WaitGroup，等待goroutine退出
 type WaitGroupWrapper struct {
 	sync.WaitGroup
 }
 
+// 传入调用函数，执行前WaitGroup加一，运行结束减一
 func (w *WaitGroupWrapper) Wrap(cb func()) {
 	w.Add(1)
 	go func() {
