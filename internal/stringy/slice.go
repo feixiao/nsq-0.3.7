b5c8007bfc9a2f551f72a64e228fb5fcd056052e
frank <feixiao2020@sina.com>
2017-04-21 10:27:24 +0800

    Update slice.go

--- internal/stringy/slice.go
+++ internal/stringy/slice.go
@@ -1,5 +1,6 @@
 package stringy
 
+// 如果string类型的a在切片中不存在，那么就添加到切片
 func Add(s []string, a string) []string {
 	for _, existing := range s {
 		if a == existing {
@@ -9,7 +10,7 @@ func Add(s []string, a string) []string {
 	return append(s, a)
 }
 
-
+// 求两个切片的并集
 func Union(s []string, a []string) []string {
 	for _, entry := range a {
 		found := false
@@ -26,6 +27,7 @@ func Union(s []string, a []string) []string {
 	return s
 }
 
+// 返回切片s中的元素(去掉重复部分)
 func Uniq(s []string) (r []string) {
 	for _, entry := range s {
 		found := false
